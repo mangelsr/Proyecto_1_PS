42aa8cc40e584cf55016c85ba1774521d60da90d
mangelsr <mangelsr@live.com>
2017-06-15 10:29:56 -0500

    Se definio algunas funciones

--- src/Lista_Siguiente.c
+++ src/Lista_Siguiente.c
@@ -3,5 +3,5 @@
 ElementoLista *Lista_Siguiente(ListaEnlazada *lista, ElementoLista *elemento);
 
 ElementoLista *Lista_Siguiente(ListaEnlazada *lista, ElementoLista *elemento){
-  
+  return NULL;
 }
--- src/Lista_Ultimo.c
+++ src/Lista_Ultimo.c
@@ -3,5 +3,8 @@
 ElementoLista *Lista_Ultimo(ListaEnlazada *lista);
 
 ElementoLista *Lista_Ultimo(ListaEnlazada *lista){
-  
+  if (lista != NULL)
+		return lista->ancla->anterior;
+	else //PREGUNTAR QUE RETORNA SI LA LISTA NO EXISTE
+		return NULL;
 }
